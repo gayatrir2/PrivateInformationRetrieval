0a198b5cdbece42dbdfd7d9f337d4c8a1dab6c9d
famr369 <felipemarplesrodriguez@gmail.com>
2026-05-02 20:22:09 -0400

    Add files via upload

---
+++ Seal_Impl/oldSEAL.h
@@ -0,0 +1,19 @@
+﻿// oldSEAL.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include "seal/seal.h"
+#include "seal/version.h"
+#include "pir.hpp"
+#include "pir_client.hpp"
+#include "pir_server.hpp"
+#include <chrono>
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+#include <random>
+#include <string>
+
+// TODO: Reference additional headers your program requires here.
